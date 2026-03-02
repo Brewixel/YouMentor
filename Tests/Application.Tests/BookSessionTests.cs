003a5d4ba082ba504b2b7db5498327cd1f9a1c00
Nikita Ovechenko <ovechenko.n.r@gmail.com>
2026-03-02 19:43:36 +0300

    test(session): Cover Create and Book with integration tests

---
+++ Tests/Application.Tests/BookSessionTests.cs
@@ -0,0 +1,220 @@
+using Application.Sessions;
+using Domain.Entities;
+using Domain.Results;
+using FluentAssertions;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+
+namespace Application.Tests;
+
+public class BookSessionTests : IntegrationTestBase
+{
+	[Fact]
+	public async Task Handle_Should_BookSession_When_EverythingIsOk()
+	{
+		// Arrange
+		var sessionId = await CreateSessionAsync();
+		var command = new Book.Command()
+		{
+			SessionId = sessionId,
+			StudentId = Guid.NewGuid(),
+		};
+		await using var setupContext = BuildContext();
+		var handler = new Book.Handler(setupContext, NullLogger<Book.Handler>.Instance);
+
+		// Act
+		var result = await handler.Handle(command, CancellationToken.None);
+
+		// Assert
+		result.IsSuccess.Should().BeTrue(result.ErrorInfo?.Message);
+
+		var session = await GetSessionAsync(sessionId);
+		session.Status.Should().Be(SessionStatus.Booked);
+	}
+
+	[Fact]
+	public async Task Handle_Should_ReturnNotFound_When_SessionDoesNotExist()
+	{
+		// Arrange
+		var command = new Book.Command()
+		{
+			SessionId = Guid.NewGuid(),
+			StudentId = Guid.NewGuid(),
+		};
+		await using var setupContext = BuildContext();
+		var handler = new Book.Handler(setupContext, NullLogger<Book.Handler>.Instance);
+
+		// Act
+		var result = await handler.Handle(command, CancellationToken.None);
+
+		// Assert
+		result.IsSuccess.Should().BeFalse();
+		result.ErrorInfo?.Type.Should().Be(ErrorType.NotFound);
+	}
+
+	[Fact]
+	public async Task Handle_Should_ReturnValidation_When_StudentIsEmpty()
+	{
+		// Arrange
+		var sessionId = await CreateSessionAsync();
+		var studentId = Guid.Empty;
+
+		await using var setup = BuildBookSetup(sessionId, studentId: studentId);
+
+		// Act
+		var result = await setup.Handler.Handle(setup.Command, CancellationToken.None);
+
+		// Assert
+		result.IsSuccess.Should().BeFalse();
+		result.ErrorInfo?.Type.Should().Be(ErrorType.Validation);
+
+		var session = await GetSessionAsync(sessionId);
+		session.Status.Should().Be(SessionStatus.Free);
+		session.StudentId.Should().BeNull();
+	}
+
+	[Fact]
+	public async Task Handle_Should_ReturnConflict_When_SessionIsAlreadyBooked()
+	{
+		// Arrange
+		var sessionId = await CreateSessionAsync();
+		await using var setup1 = BuildBookSetup(sessionId);
+		await using var setup2 = BuildBookSetup(sessionId);
+
+		// Act
+		var result1 = await setup1.Handler.Handle(setup1.Command, CancellationToken.None);
+		var result2 = await setup2.Handler.Handle(setup2.Command, CancellationToken.None);
+
+		// Assert
+		result1.IsSuccess.Should().BeTrue(result1.ErrorInfo?.Message);
+
+		result2.IsSuccess.Should().BeFalse();
+		result2.ErrorInfo?.Type.Should().Be(ErrorType.Conflict);
+
+		var checkupContext = BuildContext();
+		var session = checkupContext.Sessions.First();
+		session.StudentId.Should().Be(setup1.Command.StudentId);
+	}
+
+	[Fact]
+	public async Task Handle_Should_RetryAndSave_When_RetriesCountLessThanMax()
+	{
+		// Arrange
+		var initTasks = Enumerable.Range(1, Book.Handler.MaxRetries)
+			.Select(
+				async num =>
+				{
+					var sessionId = await CreateSessionAsync();
+					var studentId = Guid.NewGuid();
+					return new ConcurentData
+					{
+						SessionId = sessionId,
+						StudentId = studentId,
+						Setup = BuildBookSetup(sessionId,
+							logger: new FakeLogger<Book.Handler>(),
+							contextInterceptors: [new ConcurrencyExceptionInterceptor(num)]
+						),
+						RetriesCount = num
+					};
+				}
+			);
+
+		var concurentDict = (await Task.WhenAll(initTasks))
+			.ToList();
+
+		// Act
+		foreach (var concurentData in concurentDict)
+		{
+			var setup = concurentData.Setup;
+			concurentData.Result = await setup.Handler.Handle(setup.Command, CancellationToken.None);
+		}
+
+		// Assert
+		foreach(var concurentData in concurentDict)
+		{
+			var result = concurentData.Result;
+			var sessionId = concurentData.SessionId;
+			var logger = concurentData.Setup.Logger!;
+			var retriesCount = concurentData.RetriesCount;
+
+			result.IsSuccess.Should().BeTrue($"{result.ErrorInfo?.Message}. Retries count: {retriesCount}");
+			var session = await GetSessionAsync(sessionId);
+			session.Status.Should().Be(SessionStatus.Booked);
+
+			logger.Messages.Where(x => x.Contains("Concurrency conflict"))
+				.Should().HaveCount(retriesCount);
+		}
+	}
+
+	private BookSetup BuildBookSetup(Guid sessionId,
+		Guid? studentId = null,
+		FakeLogger<Book.Handler>? logger = null,
+		IInterceptor[]? contextInterceptors = null)
+	{
+		studentId ??= Guid.NewGuid();
+		contextInterceptors ??= [];
+		ILogger<Book.Handler> abstractLogger = logger != null
+			? logger
+			: NullLogger<Book.Handler>.Instance;
+
+		var command = new Book.Command()
+		{
+			SessionId = sessionId,
+			StudentId = studentId.Value,
+		};
+
+		var setupContext = BuildContext(contextInterceptors);
+		var handler = new Book.Handler(setupContext, abstractLogger);
+
+		return new BookSetup
+		{
+			Command =  command,
+			Handler = handler,
+			Context = setupContext,
+			Logger = logger
+		};
+	}
+
+	private async Task<Guid> CreateSessionAsync()
+	{
+		var sessionDto = new CreateSessionDto()
+		{
+			MentorId = Guid.NewGuid(),
+			StartTime = DateTime.UtcNow.AddDays(1),
+			Duration = TimeSpan.FromHours(1),
+		};
+		var command = new Create.Command() { Session = sessionDto };
+
+		await using var context = BuildContext();
+		var handler = new Create.Handler(context);
+
+		return (await handler.Handle(command, CancellationToken.None)).Value;
+	}
+
+	public class BookSetup : IAsyncDisposable
+	{
+		public required Book.Command Command { get; set; }
+		public required Book.Handler Handler { get; set; }
+		public required AppDbContext Context { get; set; }
+		public FakeLogger<Book.Handler>? Logger { get; set; }
+
+		public async ValueTask DisposeAsync()
+		{
+			await Context.DisposeAsync();
+		}
+	}
+
+	record ConcurentData
+	{
+		public required BookSetup Setup { get; set; }
+		public required Guid SessionId { get; set; }
+		public required Guid StudentId { get; set; }
+		public Result Result { get; set; } = null!;
+		public required int RetriesCount { get; set; }
+	}
+}
---
+++ Tests/Application.Tests/CreateSessionTests.cs
@@ -0,0 +1,80 @@
+using Application.Interfaces;
+using Application.Sessions;
+using Domain.Entities;
+using FluentAssertions;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.Tests;
+
+public class CreateSessionTests : IntegrationTestBase
+{
+	[Fact]
+	public async Task Handle_Should_CreateSession_And_SaveToDatabase()
+	{
+		// Arrange
+		var sessionDto = new CreateSessionDto()
+		{
+			MentorId = Guid.NewGuid(),
+			StartTime = DateTime.UtcNow.AddDays(1),
+			Duration = TimeSpan.FromHours(1),
+		};
+		var command = new Create.Command() { Session = sessionDto };
+		await using var setupContext = BuildContext();
+		var handler = new Create.Handler(setupContext);
+
+		// Act
+		var result = await handler.Handle(command, CancellationToken.None);
+
+		// Assert
+		result.IsSuccess.Should().BeTrue(result.ErrorInfo?.Message);
+		result.Value.Should().NotBeEmpty();
+
+		var session = await GetSessionAsync(result.Value, false);
+		session.Should().NotBeNull();
+		session.MentorId.Should().Be(sessionDto.MentorId);
+		session.StartTime.Should().BeCloseTo(sessionDto.StartTime, TimeSpan.FromMilliseconds(1));
+		session.Duration.Should().Be(sessionDto.Duration);
+	}
+
+	[Fact]
+	public async Task Handle_Should_NotSave_When_DomainValidationFails()
+	{
+		// Arrange
+		var sessionDto = new CreateSessionDto()
+		{
+			MentorId = Guid.Empty, // triggers validation error
+			StartTime = DateTime.UtcNow.AddDays(1),
+			Duration = TimeSpan.FromHours(1),
+		};
+		var command = new Create.Command() { Session = sessionDto };
+		await using var setupContext = BuildContext();
+		var handler = new Create.Handler(setupContext);
+
+		// Act
+		var result = await handler.Handle(command, CancellationToken.None);
+
+		// Assert
+		result.IsSuccess.Should().BeFalse();
+		result.ErrorInfo?.Type.Should().Be(Domain.Results.ErrorType.Validation);
+
+		await using var checkupContext = BuildContext();
+		var invalidSessions = await checkupContext.Sessions
+			.Where(x => x.MentorId == sessionDto.MentorId)
+			.ToListAsync();
+
+		invalidSessions.Should().BeEmpty();
+	}
+}
