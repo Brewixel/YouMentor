d2049d19374b5091621bf69ef4ec1fb4c96f75b3
Nikita Ovechenko <nikita.dev.job@gmail.com>
2026-07-01 21:15:12 +0400

    fix: Make runtime query generation match design-time

--- Api/Program.cs
+++ Api/Program.cs
@@ -19,7 +19,8 @@ internal class Program
 
 		var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 		builder.Services.AddDbContext<AppDbContext>(options =>
-			options.UseNpgsql(connectionString));
+			options.UseNpgsql(connectionString)
+				.UseSnakeCaseNamingConvention());
 		builder.Services.AddScoped<IAppDbContext>(provider =>
 			provider.GetRequiredService<AppDbContext>());
 
